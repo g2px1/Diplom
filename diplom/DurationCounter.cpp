1526dc6024232cf0f0d16cf840d0d1d9f24b3acc
g2px1 <kir1kir1@yandex.ru>
2023-09-30 21:28:59 +0300

    readd documentation #2

--- diplom/DurationCounter.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Kirill Zhukov on 22.09.2023.
-//
-
-#include "DurationCounter.h"
-
-uint64_t BasicDurationCounter::finishManually() {
-    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_time).count();
-}
