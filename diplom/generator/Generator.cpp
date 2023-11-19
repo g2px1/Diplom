52f047da7e24fffbd9d33c21752023560909a319
Sunaked <syrgulev-aa@mail.ru>
2023-11-19 14:56:16 +0300

    added chrono to duration; modified error output for unknown operation

--- diplom/generator/Generator.cpp
+++ diplom/generator/Generator.cpp
@@ -4,64 +4,77 @@
 
 #include "Generator.h"
 
-test::framework::Generator::Generator(uint8_t push_percantage, uint8_t pop_percantage, uint8_t read_percantage,
-                                      uint8_t write_percantage, uint64_t operations_quantity) : push(push_percantage),
-                                                                                                pop(pop_percantage), read(read_percantage), write(write_percantage), operations_q(operations_quantity) {
-    int8_t total_distribution = push_percantage + pop_percantage + read_percantage + write_percantage;
-    if (total_distribution != 100) {
-        std::cerr << "Wrong distribution: " << (int) total_distribution << "%, should be 100%";
-    }
-    std::random_device rd;
-    this->gen_engine = std::mt19937_64(rd());
+test::framework::Generator::Generator(uint8_t push_percantage,
+                                      uint8_t pop_percantage,
+                                      uint8_t read_percantage,
+                                      uint8_t write_percantage,
+                                      uint64_t operations_quantity)
+    : push(push_percantage), pop(pop_percantage), read(read_percantage),
+      write(write_percantage), operations_q(operations_quantity) {
+  int8_t total_distribution =
+      push_percantage + pop_percantage + read_percantage + write_percantage;
+  if (total_distribution != 100) {
+    std::cerr << "Wrong distribution: " << (int)total_distribution
+              << "%, should be 100%";
+  }
+  std::random_device rd;
+  this->gen_engine = std::mt19937_64(rd());
 }
 
-void test::framework::Generator::generator(bool writeLogFile, const std::string &file_name) {
-    double push_operation_q = static_cast<double>(this->operations_q * this->push) / 100;
-    double pop_operation_q = static_cast<double>(this->operations_q * this->pop) / 100;
-    double read_operation_q = static_cast<double>(this->operations_q * this->read) / 100;
-    double write_operation_q = static_cast<double>(operations_q) - push_operation_q - pop_operation_q - read_operation_q;
+void test::framework::Generator::generator(bool writeLogFile,
+                                           const std::string &file_name) {
+  double push_operation_q =
+      static_cast<double>(this->operations_q * this->push) / 100;
+  double pop_operation_q =
+      static_cast<double>(this->operations_q * this->pop) / 100;
+  double read_operation_q =
+      static_cast<double>(this->operations_q * this->read) / 100;
+  double write_operation_q = static_cast<double>(operations_q) -
+                             push_operation_q - pop_operation_q -
+                             read_operation_q;
 
-    std::vector<int16_t> operations;
-    operations.reserve(this->operations_q);
+  std::vector<int16_t> operations;
+  operations.reserve(this->operations_q);
 
-    std::discrete_distribution<> d({push_operation_q, pop_operation_q, read_operation_q, write_operation_q});
-    for (size_t i = 0; i < this->operations_q; ++i) {
-        int operationIndex = d(this->gen_engine); // Генерация индекса операции
-        operations.push_back(OP_OPERATIONS[operationIndex]);
-    }
+  std::discrete_distribution<> d(
+      {push_operation_q, pop_operation_q, read_operation_q, write_operation_q});
+  for (size_t i = 0; i < this->operations_q; ++i) {
+    int operationIndex = d(this->gen_engine); // Генерация индекса операции
+    operations.push_back(OP_OPERATIONS[operationIndex]);
+  }
 
-    // Перенос сгенерированных операций в this->operations
-    this->operations = std::move(operations);
-    if (writeLogFile) [[unlikely]] {
-        this->write_log(file_name);
-    }
+  // Перенос сгенерированных операций в this->operations
+  this->operations = std::move(operations);
+  if (writeLogFile) [[unlikely]] {
+    this->write_log(file_name);
+  }
 }
 
 void test::framework::Generator::write_log(const std::string &file_name) {
-    FILE *fd = fopen(file_name.c_str(), "wb");
-    if (!fd) {
-        std::cerr << "Log file can't be opened, reason (errno): " << errno;
-        exit(9);
-    }
+  FILE *fd = fopen(file_name.c_str(), "wb");
+  if (!fd) {
+    std::cerr << "Log file can't be opened, reason (errno): " << errno;
+    exit(9);
+  }
 
-    for (size_t i = 0; i < this->operations.size(); i++) {
-        fprintf(fd, "[%zu]: %s\n", i, this->op2s(this->operations[i]));
-    }
-    fclose(fd);
+  for (size_t i = 0; i < this->operations.size(); i++) {
+    fprintf(fd, "[%zu]: %s\n", i, this->op2s(this->operations[i]));
+  }
+  fclose(fd);
 }
 
-char* test::framework::Generator::op2s(int16_t op) {
-    switch (op) {
-        case 0x0:
-            return (char *) "PUSH";
-        case 0x1:
-            return (char *) "POP";
-        case 0x2:
-            return (char *) "READ";
-        case 0x3:
-            return (char *) "WRITE";
-        default:
-            std::cerr << "Unknown operation – exit(9)";
-            exit(9);
-    }
+char *test::framework::Generator::op2s(int16_t op) {
+  switch (op) {
+  case 0x0:
+    return (char *)"PUSH";
+  case 0x1:
+    return (char *)"POP";
+  case 0x2:
+    return (char *)"READ";
+  case 0x3:
+    return (char *)"WRITE";
+  default:
+    std::cerr << "Unknown operation" << op;
+    exit(9);
+  }
 }
--- diplom/generator/Generator.h
+++ diplom/generator/Generator.h
@@ -4,46 +4,49 @@
 
 #ifndef GENERATOR_H
 #define GENERATOR_H
-#include <random>
-#include <unistd.h>
-#include <iostream>
 #include <cerrno>
 #include <cinttypes>
+#include <iostream>
+#include <random>
+#include <unistd.h>
 
 namespace test::framework {
-    /// @brief set of operations' indexes: 0x0 - PUSH, 0x1 - POP, 0x2 - READ, 0x3 - WRITE
-    constexpr int16_t OP_OPERATIONS[4] = {0x0, 0x1, 0x2, 0x3};
+/// @brief set of operations' indexes: 0x0 - PUSH, 0x1 - POP, 0x2 - READ, 0x3 -
+/// WRITE
+constexpr int16_t OP_OPERATIONS[4] = {0x0, 0x1, 0x2, 0x3};
 
-    class Generator {
-    public:
-        Generator(uint8_t push_percantage, uint8_t pop_percantage, uint8_t read_percantage, uint8_t write_percantage,
-                  uint64_t operations_quantity);
+class Generator {
+public:
+  Generator(uint8_t push_percantage, uint8_t pop_percantage,
+            uint8_t read_percantage, uint8_t write_percantage,
+            uint64_t operations_quantity);
 
-        /// @brief generates set of operations
-        /// @param writeLogFile flag is <b>true</b> if you want to write logs to a file, otherwise <b>false</b>
-        void generator(bool writeLogFile, const std::string &file_name = "default_log_file.txt");
+  /// @brief generates set of operations
+  /// @param writeLogFile flag is <b>true</b> if you want to write logs to a
+  /// file, otherwise <b>false</b>
+  void generator(bool writeLogFile,
+                 const std::string &file_name = "default_log_file.txt");
 
-        void write_log(const std::string&file_name);
+  void write_log(const std::string &file_name);
 
-        char *op2s(int16_t op);
+  char *op2s(int16_t op);
 
-    private:
-        std::mt19937_64 gen_engine;
+private:
+  std::mt19937_64 gen_engine;
 
-        /// @brief abstract name for operations such as `enqueue` etc.
-        uint8_t push;
-        /// @brief abstract name for operations such as `dequeue` etc.
-        uint8_t pop;
-        /// @brief abstract name for operations such as `at`, operator `[]` etc.
-        uint8_t read;
-        /// @brief abstract name for operations such as `insert` etc.
-        uint8_t write;
-        /// @brief operation quantity to be generated
-        uint64_t operations_q;
-        /// @brief operations sequence
-        std::vector<int16_t> operations;
-    };
-}; // test::framework
+  /// @brief abstract name for operations such as `enqueue` etc.
+  uint8_t push;
+  /// @brief abstract name for operations such as `dequeue` etc.
+  uint8_t pop;
+  /// @brief abstract name for operations such as `at`, operator `[]` etc.
+  uint8_t read;
+  /// @brief abstract name for operations such as `insert` etc.
+  uint8_t write;
+  /// @brief operation quantity to be generated
+  uint64_t operations_q;
+  /// @brief operations sequence
+  std::vector<int16_t> operations;
+};
+}; // namespace test::framework
 
-
-#endif //GENERATOR_H
+#endif // GENERATOR_H
