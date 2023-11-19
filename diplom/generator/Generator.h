bde97baedbf7e7946071633486c48cc6300c2575
g2px1 <kir1kir1@yandex.ru>
2023-11-19 13:25:14 +0300

    rewrite generator

---
+++ diplom/generator/Generator.h
@@ -0,0 +1,49 @@
+//
+// Created by Kirill Zhukov on 19.11.2023.
+//
+
+#ifndef GENERATOR_H
+#define GENERATOR_H
+#include <random>
+#include <unistd.h>
+#include <iostream>
+#include <cerrno>
+#include <cinttypes>
+
+namespace test::framework {
+    /// @brief set of operations' indexes: 0x0 - PUSH, 0x1 - POP, 0x2 - READ, 0x3 - WRITE
+    constexpr int16_t OP_OPERATIONS[4] = {0x0, 0x1, 0x2, 0x3};
+
+    class Generator {
+    public:
+        Generator(uint8_t push_percantage, uint8_t pop_percantage, uint8_t read_percantage, uint8_t write_percantage,
+                  uint64_t operations_quantity);
+
+        /// @brief generates set of operations
+        /// @param writeLogFile flag is <b>true</b> if you want to write logs to a file, otherwise <b>false</b>
+        void generator(bool writeLogFile, const std::string &file_name = "default_log_file.txt");
+
+        void write_log(const std::string&file_name);
+
+        char *op2s(int16_t op);
+
+    private:
+        std::mt19937_64 gen_engine;
+
+        /// @brief abstract name for operations such as `enqueue` etc.
+        uint8_t push;
+        /// @brief abstract name for operations such as `dequeue` etc.
+        uint8_t pop;
+        /// @brief abstract name for operations such as `at`, operator `[]` etc.
+        uint8_t read;
+        /// @brief abstract name for operations such as `insert` etc.
+        uint8_t write;
+        /// @brief operation quantity to be generated
+        uint64_t operations_q;
+        /// @brief operations sequence
+        std::vector<int16_t> operations;
+    };
+}; // test::framework
+
+
+#endif //GENERATOR_H
--- diplom/main.cpp
+++ diplom/main.cpp
@@ -1,29 +1,8 @@
 #include <iostream>
-#include <functional>
-#include "PRNG.h"
-#include "ContainerTest.h"
-#include "ContainerFunctions.h"
-#include "Pool.h"
-#include "ContainerPreparer.h"
+#include "generator/Generator.h"
 
 int main() {
-    sequence::AvailableFunctionsBuilder availableFunctionsBuilder{};
-    sequence::AvailableFunctions availableFunctions = availableFunctionsBuilder.addAll()->build();
-
-    PRNG prng(1);
-
-    std::vector<int64_t> containerToTest;
-    ContainerPreparer<std::vector<int64_t>> containerPreparer(availableFunctions);
-    SequenceContainerTester<std::vector<int64_t>, int64_t> sequenceContainerTester =
-            SequenceContainerTester<std::vector<int64_t>, int64_t>(containerToTest, containerPreparer, 10000000);
-
-    std::thread th([&sequenceContainerTester] {
-        Pool<SequenceContainerTester<std::vector<int64_t>, int64_t>, 4> pool(sequenceContainerTester);
-        pool.startThreads();
-        pool.joinThreads();
-    });
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    th.join();
+    test::framework::Generator generator(10, 10, 40, 40, 100);
+    generator.generator(true);
     return 0;
 }
